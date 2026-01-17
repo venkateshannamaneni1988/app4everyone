f80fe821c5acc9e3808826db1bd831030fa4a09e
venkateshannamaneni1988 <venkatesh.annamaneni@outlook.com>
2026-01-17 17:45:10 +0530

    Update service worker cache version to v12

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "app4everyone-v11"; // 🔴 CHANGE VERSION EVERY UPDATE
+const CACHE_NAME = "app4everyone-v12"; // 🔴 CHANGE VERSION EVERY UPDATE
 
 self.addEventListener("install", event => {
   self.skipWaiting(); // activate immediately
@@ -36,4 +36,5 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
