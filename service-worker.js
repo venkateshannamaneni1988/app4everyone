1decc49940c38f8d07065a5eb27751086fadd4d5
venkateshannamaneni1988 <venkatesh.annamaneni@outlook.com>
2026-01-18 07:32:40 +0530

    Update CACHE_NAME to version 25

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "app4everyone-v24"; // 🔴 CHANGE VERSION EVERY UPDATE
+const CACHE_NAME = "app4everyone-v25"; // 🔴 CHANGE VERSION EVERY UPDATE
 
 self.addEventListener("install", event => {
   self.skipWaiting(); // activate immediately
@@ -36,6 +36,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
